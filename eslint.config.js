ccf516949990748edfaf4ecc0dbd399c9ca2766a
sei-12 <sei.t11@icloud.com>
2025-01-30 15:44:58 +0900

    add vitest & eslint

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    { ignores: ["dist"] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ["src/**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                tsconfigRootDir: ".",
+                project: "./tsconfig.json", // tsconfig.jsonのパス
+            },
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+
+        // 基準
+        // warn : 良くないコード
+        // error: 動かないコード
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "@typescript-eslint/no-namespace": "off",
+            "react-hooks/exhaustive-deps": "off",
+            "@typescript-eslint/consistent-type-imports": "warn",
+            "prefer-const": "warn",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/strict-boolean-expressions": [
+                "warn",
+                {
+                    allowString: false,
+                    allowNumber: false,
+                    allowNullableObject: false,
+                },
+            ],
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
+    }
+);
